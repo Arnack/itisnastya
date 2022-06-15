036de398686ae2e008fbdd7065807b474008692e
Grigorii Orlov <grigorii@authentise.com>
2022-06-16 01:53:08 +0300

    pjsn updtd

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('/Users/utrom/Downloads/Retro/retro/node_modules/gatsby-plugin-google-fonts/gatsby-ssr'),
+      plugin: require('/Users/utrom/Documents/dev/nastya/itisnastya/node_modules/gatsby-plugin-google-fonts/gatsby-ssr'),
       options: {"plugins":[],"fonts":["Poppins:300,400,400,700,800,900"],"display":"swap"},
     },{
-      plugin: require('/Users/utrom/Downloads/Retro/retro/node_modules/gatsby-plugin-offline/gatsby-ssr'),
+      plugin: require('/Users/utrom/Documents/dev/nastya/itisnastya/node_modules/gatsby-plugin-offline/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/utrom/Downloads/Retro/retro/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/utrom/Documents/dev/nastya/itisnastya/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,12 +1,12 @@
 const { hot } = require("react-hot-loader/root")
 
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/utrom/Downloads/Retro/retro/.cache/dev-404-page.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("/Users/utrom/Downloads/Retro/retro/src/pages/404.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/utrom/Downloads/Retro/retro/src/pages/index.js")))
+  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/utrom/Documents/dev/nastya/itisnastya/node_modules/gatsby-plugin-offline/app-shell.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/utrom/Documents/dev/nastya/itisnastya/src/pages/404.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/utrom/Documents/dev/nastya/itisnastya/src/pages/index.js")))
 }
 
